e6bd28b6a6935f842b54c181dd180ca0e3279557
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2021-02-22 11:45:09 +0300

    remove redundant strings

--- GodSimulator.py
+++ GodSimulator.py
@@ -6,17 +6,11 @@ class GodSimulator:
 
     @staticmethod
     def have_to_die(p: BacteriaParameters):
-        if p.p_for_death > random():
-            return True
-        else:
-            return False
+        return p.p_for_death > random()
 
     @staticmethod
     def have_to_reproduct(p: BacteriaParameters):
-        if p.p_for_reproduction > random():
-            return True
-        else:
-            return False
+        return p.p_for_reproduction > random()
 
     @staticmethod
     def get_child_parameters(p: BacteriaParameters):
