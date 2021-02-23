72fc4efefa55bb13016aad0e9cea44a7c4b537d0
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2021-02-23 21:43:16 +0300

    правка второй версии
    добавлены возвращаемые значения, изменены некоторые имена методов на более корректные, константы вынесены как отдельные поля класса

--- Bacteria.py
+++ Bacteria.py
@@ -2,8 +2,8 @@ import BacteriaParameters
 from GodSimulator import GodSimulator
 
 
-def create_child(p: BacteriaParameters):
-    return Bacteria(p)
+def create_bacteria(**kwargs) -> Bacteria:
+    return Bacteria(kwargs)
 
 
 class Bacteria:
@@ -12,17 +12,17 @@ class Bacteria:
         self.is_alive = True
         self.parameters = p
 
-    def iteration(self):
+    def iteration(self) -> list[Bacteria]:
         if not self.is_alive:
-            return None
+            return []
         else:
             if not GodSimulator.have_to_die(self.parameters):
                 children = list()
                 while GodSimulator.have_to_reproduct(self.parameters):
                     child_parameters = GodSimulator.get_child_parameters(self.parameters)
-                    children.append(create_child(child_parameters))
+                    children.append(Bacteria(child_parameters))
                 self.parameters.lived_time += 1
                 return children
             else:
                 self.is_alive = False
-                return None
+                return []
--- GodSimulator.py
+++ GodSimulator.py
@@ -3,33 +3,38 @@ from random import uniform, random, randint
 import BacteriaParameters
 
 
-def probability_parameter(parent_parameter: float):
-    probability_variation = uniform(-0.1, 0.1)
-    parameter = parent_parameter + probability_variation
-    if parameter > 1:
-        parameter = 1
-    return parameter
-
-
-def choose_child_parameters(parent_parameters: BacteriaParameters):
-    child_parameters = copy.deepcopy(parent_parameters)
-    child_parameters.genome.p_for_death = probability_parameter(parent_parameters.genome.p_for_death)
-    child_parameters.genome.p_for_reproduction = probability_parameter(parent_parameters.genome.p_for_reproduction)
-    child_parameters.genome.max_n = randint(0, 15)
-    return child_parameters
-
-
 class GodSimulator:
 
+    MAX_PROBABILITY = 1
+    VARIATION_PROBABILITY_BORDERS = [-0.1, 0.1]
+    LIVES_BORDERS = [0, 15]
+
+
     @staticmethod
-    def have_to_die(p: BacteriaParameters):
+    def __probability_parameter(parent_parameter: float) -> float:
+        probability_variation = uniform(*VARIATION_PROBABILITY_BORDERS)
+        parameter = parent_parameter + probability_variation
+        if parameter > MAX_PROBABILITY:
+            parameter = MAX_PROBABILITY
+        return parameter
+
+    @staticmethod
+    def __choose_child_parameters(parent_parameters: BacteriaParameters) -> BacteriaParameters:
+        child_parameters = copy.deepcopy(parent_parameters)
+        child_parameters.genome.p_for_death = probability_parameter(parent_parameters.genome.p_for_death)
+        child_parameters.genome.p_for_reproduction = probability_parameter(parent_parameters.genome.p_for_reproduction)
+        child_parameters.genome.max_n = randint(*LIVES_BORDERS)
+        return child_parameters
+
+    @staticmethod
+    def have_to_die(p: BacteriaParameters) -> bool:
         ratio = (p.genome.max_n - p.lived_time) / p.genome.max_n
         return p.p_for_death > uniform(0, ratio)
 
     @staticmethod
-    def have_to_reproduct(p: BacteriaParameters):
+    def have_to_reproduct(p: BacteriaParameters) -> bool:
         return p.genome.p_for_reproduction > random()
 
     @staticmethod
-    def get_child_parameters(p: BacteriaParameters):
+    def get_child_parameters(p: BacteriaParameters) -> BacteriaParameters:
         return choose_child_parameters(p)
