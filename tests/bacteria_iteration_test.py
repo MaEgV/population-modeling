c310d7236e33f10a5f06cdb4a4bd87cdc40b8fc9
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2021-03-13 22:45:54 +0300

    Fix tests after reorganisation

--- tests/bacteria_iteration_test.py
+++ tests/bacteria_iteration_test.py
@@ -1,27 +1,31 @@
 import pytest
-from population_modeling import bacteria, population, Selector, ExternalFactors
-from population_modeling.bacteria import iteration
+from population_modeling import bacteria, ExternalFactors
+from population_modeling.bacteria_iteration import iterate
+from population_modeling.mutations import NormalMutations
+from population_modeling.selector import Selector
 
 
 class Case:
     def __init__(self, name, population_max, antagonism, overpopulation, max_life_time, p_for_death,
-                 p_for_reproduction):
+                 p_for_reproduction, mutation_mode):
         self.name = name
         self.population_max = population_max
         self.antagonism = antagonism
         self.overpopulation = overpopulation
         self.bacteria = bacteria.create_bacteria(max_life_time, p_for_death, p_for_reproduction)
+        self.mutation_mode = mutation_mode
 
 
 TEST_CASES_BACTERIA_ITERATION = [Case(name="Simple case", population_max=10, antagonism=0, overpopulation=0,
-                                      max_life_time=5, p_for_death=0.1, p_for_reproduction=0.1)]
+                                      max_life_time=5, p_for_death=0.1, p_for_reproduction=0.1,
+                                      mutation_mode=NormalMutations())]
 
 
 @pytest.mark.parametrize('bacteria_iteration_alive', TEST_CASES_BACTERIA_ITERATION, ids=str)
 def test_iterator_alive(bacteria_iteration_alive: Case) -> None:
-    result = iteration(population.Selector(
-        ExternalFactors(bacteria_iteration_alive.antagonism,
-                        bacteria_iteration_alive.overpopulation)), bacteria_iteration_alive.bacteria)
+    result = iterate(Selector(
+        ExternalFactors(bacteria_iteration_alive.antagonism, bacteria_iteration_alive.overpopulation)),
+        bacteria_iteration_alive.mutation_mode, bacteria_iteration_alive.bacteria)
     assert len(result) >= 0
 
 
@@ -29,5 +33,5 @@ def test_iterator_alive(bacteria_iteration_alive: Case) -> None:
 def test_iterator_dead(bacteria_iteration_dead: Case) -> None:
     bacteria_iteration_dead.bacteria.is_alive = False
     with pytest.raises(BaseException):
-        iteration(Selector(ExternalFactors(bacteria_iteration_dead.antagonism,
-                                           bacteria_iteration_dead.overpopulation)), bacteria_iteration_dead.bacteria)
+        iterate(Selector(ExternalFactors(bacteria_iteration_dead.antagonism, bacteria_iteration_dead.overpopulation)),
+                bacteria_iteration_dead.mutation_mode, bacteria_iteration_dead.bacteria)
