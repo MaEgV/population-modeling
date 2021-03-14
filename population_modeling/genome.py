36b290fc2d048d8c172c65be056ed0c8f990cd12
MaEgV <egor.marin2017@yandex.ru>
2021-03-14 11:35:22 +0300

    Decorative fix

--- population_modeling/genome.py
+++ population_modeling/genome.py
@@ -21,7 +21,7 @@ class Genome:
 
     """
 
-    def __init__(self, max_life_time_: int, p_for_death_: float, p_for_reproduction_: float):
-        self.max_life_time = max_life_time_
-        self.p_for_death = p_for_death_
-        self.p_for_reproduction = p_for_reproduction_
+    def __init__(self, max_life_time: int, p_for_death: float, p_for_reproduction: float):
+        self.max_life_time = max_life_time
+        self.p_for_death = p_for_death
+        self.p_for_reproduction = p_for_reproduction
