98200ef3f83578fab78bd9269831c402e2724dd6
MaEgV <60978225+MaEgV@users.noreply.github.com>
2021-05-27 19:26:26 +0300

    settings fix

--- src/server/settings.py
+++ src/server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ SECRET_KEY = 'django-insecure-*)td%hca^weqx=u-jtx-^0-n@d+=-tyk7n1lye%@-spmc7!aer
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-HEROKU = False
+HEROKU = True
 ALLOWED_HOSTS = ['population-modeling.herokuapp.com', '127.0.0.1']
 
 
